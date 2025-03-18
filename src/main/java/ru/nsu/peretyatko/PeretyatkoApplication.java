aa8384dc4497c61d2faa6d920146af65431f8d2a
i.peretyatko <i.peretyatko@g.nsu.ru>
2025-03-18 13:45:26 +0700

    init

---
+++ src/main/java/ru/nsu/peretyatko/PeretyatkoApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.peretyatko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeretyatkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeretyatkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nsu/peretyatko/PeretyatkoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.peretyatko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeretyatkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
