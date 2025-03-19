1081aa6d9754ceb9339b50ed9aca8e8b8b3e7b73
i.peretyatko <i.peretyatko@g.nsu.ru>
2025-03-19 14:00:28 +0700

    fix

--- src/main/java/ru/nsu/peretyatko/config/MailConfig.java
+++ src/main/java/ru/nsu/peretyatko/config/MailConfig.java
@@ -3,12 +3,14 @@ package ru.nsu.peretyatko.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import ru.nsu.peretyatko.config.properties.MailProperties;
 
 @Configuration
 @RequiredArgsConstructor
+@PropertySource("classpath:mail.properties")
 public class MailConfig {
     private final MailProperties mailProperties;
 
